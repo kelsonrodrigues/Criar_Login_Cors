91994d611145cb23257377abe9d14db2a25a0d9a
kelson85 <kelson07cursos@gmail.com>
2024-09-25 20:07:12 -0300

    Feat: validatio Form

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2d21fef3b4014aa1ad4166a2d1fd14618debc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49bcf3fde68a4ea768daa59db733d80a913d5fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
